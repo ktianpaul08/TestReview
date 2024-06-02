93e534316785bb9a7f52d83a3f05dd7fc22331ea
KRISTIAN PAUL <KRISTIAN PAUL@LAPTOP-GM6POR0P.mshome.net>
2024-06-02 15:52:39 +0800

    Updated DriverSetup class

--- TestReviewer/src/test/java/DriverSetup.java
+++ TestReviewer/src/test/java/DriverSetup.java
@@ -8,23 +8,23 @@ public class DriverSetup {
 	public DriverSetup() {
 
 	}
-	public WebDriver setDriver(String browser) {
-		switch(browser){
-			
-			case "Edge":
-			System.setProperty("webdriver.edge.driver","D:\\OneDrive\\edgedriver_win64\\msedgedriver.exe");
-			driver = new EdgeDriver();	
-			
-			break;
-			case "Chrome":
-				//System.setProperty("webdriver.edge.driver","D:\\OneDrive\\edgedriver_win64\\msedgedriver.exe");
-//				driver = new ChromeDriver();
-				System.out.println("asdasd");
-			break;
-		}
-		return driver;
-		
-	}
+//	public WebDriver setDriver(String browser) {
+//		switch(browser){
+//			
+//			case "Edge":
+//			System.setProperty("webdriver.edge.driver","D:\\OneDrive\\edgedriver_win64\\msedgedriver.exe");
+//			driver = new EdgeDriver();	
+//			
+//			break;
+//			case "Chrome":
+//				//System.setProperty("webdriver.edge.driver","D:\\OneDrive\\edgedriver_win64\\msedgedriver.exe");
+////				driver = new ChromeDriver();
+//				System.out.println("asdasd");
+//			break;
+//		}
+//		return driver;
+//		
+//	}
 	
 	public WebDriver setDriver2() {
 		System.setProperty("webdriver.edge.driver","D:\\OneDrive\\edgedriver_win64\\msedgedriver.exe");
